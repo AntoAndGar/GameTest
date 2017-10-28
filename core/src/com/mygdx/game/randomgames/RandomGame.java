22fbdc70c083a851110500930bebe276a415f3c5
AntoAndGar <32210935+AntoAndGar@users.noreply.github.com>
2017-10-28 17:34:12 +0200

    Do things
    DESTROY WEBAPP!!!
    write some code in screen and add a message, it font conflicts with webapp
    (we need to fix)

--- core/src/com/mygdx/game/randomgames/RandomGame.java
+++ core/src/com/mygdx/game/randomgames/RandomGame.java
@@ -1,24 +1,31 @@
 package com.mygdx.game.randomgames;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.randomgames.screens.MainGameScreen;
 
-public class RandomGame extends ApplicationAdapter 
+public class RandomGame extends Game//extends ApplicationAdapter
 {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
 	
 	public static final MainGameScreen _mainGameScreen = new MainGameScreen();
 	
 	@Override
 	public void create() 
 	{
-//		setScreen(_mainGameScreen);
+		setScreen(_mainGameScreen);
 		batch = new SpriteBatch();
+		font = new BitmapFont(Gdx.files.internal("verdana39.fnt"),
+		Gdx.files.internal("verdana39.png"), false);
+		font.setColor(Color.BLUE);
 		img = new Texture("Truncated_octahedra.jpg");
 	}
 
@@ -28,14 +35,16 @@ public class RandomGame extends ApplicationAdapter
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		font.draw(batch, "~~ Shit ~~", 400, 400);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose() 
 	{
-//		_mainGameScreen.dispose();
+		_mainGameScreen.dispose();
 		batch.dispose();
+		font.dispose();
 		img.dispose();
 	}
 }
---
+++ core/src/com/mygdx/game/randomgames/screens/GameScreen.java
@@ -0,0 +1,6 @@
+package com.mygdx.game.randomgames.screens;
+
+public class GameScreen 
+{
+
+}
