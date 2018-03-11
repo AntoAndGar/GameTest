4aeacc6bc860f054852328d0fdb977650f680b2e
AntoAndGar <32210935+AntoAndGar@users.noreply.github.com>
2018-03-11 18:38:11 +0100

    some partial implementation

--- core/src/com/mygdx/game/randomgames/screens/MainGameScreen.java
+++ core/src/com/mygdx/game/randomgames/screens/MainGameScreen.java
@@ -1,6 +1,16 @@
 package com.mygdx.game.randomgames.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.randomgames.Entity;
+import com.mygdx.game.randomgames.MapManager;
+import com.mygdx.game.randomgames.PlayerController;
 
 /**
  * Will be the primary gameplay screen that displays 
@@ -11,21 +21,101 @@ import com.badlogic.gdx.Screen;
  */
 public class MainGameScreen extends GameScreen implements Screen
 {
-	public void dispose() 
-	{
-		
+	private static final String TAG = MainGameScreen.class.getSimpleName();
+	
+	private static class VIEWPORT {
+		static float viewportWidth;
+		static float viewportHeight;
+		static float virtualWidth;
+		static float virtualHeight;
+		static float physicalWidth;
+		static float physicalHeight;
+		static float aspectRatio;
 	}
-
+	
+	private PlayerController _controller;
+	private TextureRegion _currentPlayerFrame;
+	private Sprite _currentPlayerSprite;
+	
+	private OrthogonalTiledMapRenderer _mapRenderer = null;
+	private OrthographicCamera _camera = null;
+	private static MapManager _mapMgr;
+	
+	public MainGameScreen(){
+		_mapMgr = new MapManager();
+	}
+	
+	private static Entity _player;
+	
+	/**
+	 * show the new screen
+	 */
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
+		//camera setup
+		setupViewport(10, 10);
+		
+		//get the current size
+		_camera = new OrthographicCamera();
+		//false = positive y facing up
+		_camera.setToOrtho(false, VIEWPORT.viewportWidth, VIEWPORT.viewportHeight);
+		
+		_mapRenderer = new OrthogonalTiledMapRenderer(_mapMgr.getCurrentMap(), MapManager.UNIT_SCALE);
+		_mapRenderer.setView(_camera);
+		
+		Gdx.app.debug(TAG, "UnitScale value is: " + _mapRenderer.getUnitScale());
+		
+		_player = new Entity();
+		_player.init(_mapMgr.getPlayerStartUnitScaled().x, _mapMgr.getPlayerStartUnitScaled().y);
+		
+		_currentPlayerSprite =_player.getFrameSprite();
+		
+		_controller = new PlayerController(_player);
+		Gdx.input.setInputProcessor(_controller);
 		
 	}
 
+	/**
+	 * Will be called every frame, and is the primary location for
+	 * rendering, updating, and checking for collisions in the game lifecycle. First, we will
+	 * make sure that we lock the viewport (camera position) to the current position of
+	 * our player character so that the player is always in the middle of the screen. We will
+	 * then check whether the player has activated a portal.
+	 * We will also check for collisions with
+	 * the collision layer of the map, and if there are collisions, then we will not update
+	 * the player’s position. We make sure that we update the camera information in the
+	 * OrthogonalTiledMapRenderer object and then render the TiledMap object first
+	 * because, as mentioned previously, the order in which you draw your objects matter.
+	 * Finally, we will draw the character to the screen, making sure to use the getBatch()
+	 * call for when we have numerous objects to update. By drawing in a batch update, the
+	 * overhead of updating the textures will be minimal since the GPU will consume the
+	 * texture updates at one time instead of constantly throttling between updating and
+	 * rendering separate textures.
+	 */
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		//Preferable to lock and center the _camera to the player’s position
+		_camera.position.set(_currentPlayerSprite.getX(), _currentPlayerSprite.getY(), 0f);
+		_camera.update();
+		
+		_player.update(delta);
+		_currentPlayerFrame = _player.getFrame();
+		
+		updatePortalLayerActivation(_player.boundingBox);
+		
+		if( !isCollisionWithMapLayer(_player.boundingBox) ) 
+			_player.setNextPositionToCurrent();
+		_controller.update(delta);
+		
+		_mapRenderer.setView(_camera);
+		_mapRenderer.render();
+		
+		_mapRenderer.getBatch().begin();
+		_mapRenderer.getBatch().draw(_currentPlayerFrame, _currentPlayerSprite.getX(), _currentPlayerSprite.getY(), 1, 1);
+		_mapRenderer.getBatch().end();
 	}
 
 	@Override
@@ -46,9 +136,67 @@ public class MainGameScreen extends GameScreen implements Screen
 		
 	}
 
+	/**
+	 * hide the actual screen
+	 */
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public void dispose() 
+	{
+		_player.dispose();
+		_controller.dispose();
+		Gdx.input.setInputProcessor(null);
+		_mapRenderer.dispose();
+	}
+	
+	/**
+	 * helps with the bookkeeping of our inner class 
+	 * VIEWPORT. This is simply a convenience class for maintaining all the parameters
+	 * that compose our viewport for the camera. This class will also account for the
+	 * skewing that can occur depending on the width to height ratio and will update
+	 * the values accordingly
+	 * @param width
+	 * @param height
+	 */
+	private void setupViewport(int width, int height) {
+		//Make the viewport a percentage of the total display area
+		VIEWPORT.virtualWidth = width;
+		VIEWPORT.virtualHeight = height;
+		
+		//Current viewport dimensions
+		VIEWPORT.viewportWidth = VIEWPORT.virtualWidth;
+		VIEWPORT.viewportHeight = VIEWPORT.virtualHeight;
+		
+		//pixel dimensions of display
+		VIEWPORT.physicalWidth = Gdx.graphics.getWidth();
+		VIEWPORT.physicalHeight = Gdx.graphics.getHeight();
+		
+		//aspect ratio for current viewport
+		VIEWPORT.aspectRatio = (VIEWPORT.virtualWidth / VIEWPORT.viewportHeight);
+		
+		//update viewport if there could be skewing
+		if( VIEWPORT.physicalWidth / VIEWPORT.physicalHeight >= VIEWPORT.aspectRatio ) {
+			//Letterbox left and right
+			VIEWPORT.viewportWidth = VIEWPORT.viewportHeight * (VIEWPORT.physicalWidth/VIEWPORT.physicalHeight);
+			VIEWPORT.viewportHeight = VIEWPORT.virtualHeight;
+		} 
+		else {
+		//letterbox above and below
+			VIEWPORT.viewportWidth = VIEWPORT.virtualWidth;
+			VIEWPORT.viewportHeight = VIEWPORT.viewportWidth * (VIEWPORT.physicalHeight/VIEWPORT.physicalWidth);
+		}
+		
+		Gdx.app.debug(TAG, "WorldRenderer: virtual: (" + VIEWPORT.virtualWidth + "," + VIEWPORT.virtualHeight + ")" );
+		Gdx.app.debug(TAG, "WorldRenderer: viewport: (" + VIEWPORT.viewportWidth + "," + VIEWPORT.viewportHeight + ")" );
+		Gdx.app.debug(TAG, "WorldRenderer: physical: (" + VIEWPORT.physicalWidth + "," + VIEWPORT.physicalHeight + ")" );
+	}
+	
+	private boolean isCollisionWithMapLayer(Rectangle boundingBox) {
+		//TODO: implementig missing stuff
 	}
 }
